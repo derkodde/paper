e900667cd1e454dcb0d3ba0d0fa84d4cd58bd25e
derkodde <basti.kotte@gmail.com>
2016-06-19 13:17:06 +0200

    fertig

--- stack.php
+++
@@ -1,7 +0,0 @@
-<?php
-$page = "stack";
-$path = $_SERVER['DOCUMENT_ROOT'].'/paper/';
-// $currPage = basename(__FILE__, '.php');
-// $content = file_get_contents($path.'content/'.$page.'.html');
-include_once $path.'/default-template.php';
-?>
